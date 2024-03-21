0eb363d1590e4f9d043abd45e8abc7e2e1be2d22
Ye (Shawn) Wang <wang77289271@gmail.com>
2024-03-21 10:10:26 -0600

    fix build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,9 +10,9 @@ import {
 } from '@/lib/utils'
 import { NextResponse } from 'next/server'
 
-// export const maxDuration = 300
-// export const dynamic = 'force-dynamic'
-// export const revalidate = 0
+export const maxDuration = 300
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 export async function GET() {
   try {
